f121bf060b74237c3e407e8e75606b71c8138a5f
jeunessesept <b.jiacinto@gmail.com>
2022-10-18 19:59:13 +0200

    up

--- events/event_script.js
+++ events/event_script.js
@@ -9,5 +9,6 @@ const dispWrapp = document.getElementsByClassName("displayedsquare-wrapper")
 
 const newsquare = document.createElement("div")
 newsquare.classList.add("displayedsquare")
+newsquare.style.background = "#00ff00";
 
 dispWrapp[0].appendChild(newsquare);
