ce0ce1698a841abe5fbd318f5ad771b6873d9033
jeunessesept <b.jiacinto@gmail.com>
2022-10-19 16:54:16 +0200

    struggle

--- events/event_script.js
+++ events/event_script.js
@@ -1,14 +1,112 @@
-// const displayedsquare = () => {
-//     let square = document.createElement("displayedsquare"); 
+const dispWrapp = document.querySelector(".displayedsquare-wrapper");
+const logAct = document.querySelector("ul");
+const action = document.querySelector(".actionsquare-wrapper");
+const lime = action.children[0];
+const violet = action.children[1];
+const orange = action.children[2];
+
+
+
+// const colorChoice = () => {
+//   const actions = document.querySelectorAll("actionsquare-wrapper");
+
+//   for (let _element of actions){
+//     actions.addEventListener("click", (a) => {
+//         alert("hello");
+//     });
     
+//   };
 // }
 
-// displayedsquare()
+// colorChoice()
+/// creating new squares
 
-const dispWrapp = document.getElementsByClassName("displayedsquare-wrapper")
+ const newlime = () => {
+   const newsquarelime = document.createElement("div");
+   newsquarelime.classList.add("displayedsquare");
+   newsquarelime.style.background = "#00ff00";
 
-const newsquare = document.createElement("div")
-newsquare.classList.add("displayedsquare")
-newsquare.style.background = "#00ff00";
+   dispWrapp.appendChild(newsquarelime);
+ };
 
-dispWrapp[0].appendChild(newsquare);
+ const newviolet = () => {
+   const newsquareviolet = document.createElement("div");
+   newsquareviolet.classList.add("displayedsquare");
+   newsquareviolet.style.background = "#ff00ff";
+
+   dispWrapp.appendChild(newsquareviolet);
+ };
+
+ const neworange = () => {
+   const newsquareorange = document.createElement("div");
+   newsquareorange.classList.add("displayedsquare");
+   newsquareorange.style.background = "#ffa500";
+
+   dispWrapp.appendChild(newsquareorange);
+ };
+
+//// creating new log
+
+ const newLog = () => {
+   const liAct = document.createElement("li");
+
+   liAct.textContent = "created a new square";
+
+   logAct.appendChild(liAct);
+ };
+
+
+ // add eventlistener
+
+// lime.addEventListener("click", (l) => {
+//   newlime();
+//   newLog();
+// });
+
+// violet.addEventListener("click", (v) => {
+//   newviolet();
+//   newLog();
+// });
+
+// orange.addEventListener("click", (o) => {
+//   neworange();
+//   newLog();
+// });
+const _initTime = Date.now()
+
+const getElapsedTime = () => {
+  return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
+}
+
+// const clickOnSquare = (e) => {
+//     console.log(e.target.classList[1])
+//     console.log(getElapsedTime())
+//   }
+
+
+const newSquare = () => {
+    const newsquare = document.createElement("div");
+    newsquare.classList.add("displayedsquare");
+
+    
+ 
+    dispWrapp.appendChild(newsquare);
+}
+
+
+
+const newColSquare = () => {
+    const actions = document.querySelectorAll(".actionsquare");
+
+    actions.forEach((action) => {
+        if (action.classList[1]){
+            action.addEventListener("click", () => {
+                console.log(action.classList[1])
+                
+            });
+        }
+    });
+}
+
+
+    newColSquare()
